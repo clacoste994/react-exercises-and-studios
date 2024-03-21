4f86d4dcc6cfc56af41116228de5208a3772747d
Christopher LaCoste <clacoste994@gmail.com>
2024-03-20 22:21:49 -0500

    stuffexercise

--- part1/exercises/src/components/BookList.jsx
+++ part1/exercises/src/components/BookList.jsx
@@ -1,15 +1,15 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "List of Books";
+   let book1 = "https://skyryedhttps://skyryedesign.com/wp-content/uploads/2016/04/56c6f9bcdbf95-cover-books-design-illustrations.jpgesign.com/wp-content/uploads/2016/04/56c6f9bcdbf95-cover-books-design-illustrations.jpg";
+   let book2 = "https://skyryedesign.com/wp-content/uploads/2016/04/56c6f9b7efad5-cover-books-design-illustrations.jpg://skyryedesign.com/wp-content/uploads/2016/04/56c6f9b7efad5-cover-books-design-illustrations.jpg";
+   let book3 = "https://skyryedesign.com/wp-content/uploads/2016/04/The-Busker-by-Leo-Nickolls-cover-books-design-illustrations.jpg";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src={book1} alt="Was it a fox?" />
+         <img src={book2} alt="This one had fire!" />
+         <img src={book3} alt="Looked like art on the cover?" />
       </div>      
    );
 }
---
+++ part1/exercises/src/components/HobbyLinks.jsx
@@ -0,0 +1,10 @@
+function HobbyLinks() {
+    return(
+        <div>
+        const hobbyLinks = ["stuff", "stuff", "more stuff"] 
+        <a href = {hobbyLinks[1]}>yup!</a>
+        </div>
+    )
+}
+
+export default HobbyLinks
